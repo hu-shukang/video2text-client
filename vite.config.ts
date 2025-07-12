033f9d122ea676f0b6aec35bc0dad9b1c3ba0c31
胡书康 <hushukang@gmail.com>
2025-07-12 23:18:07 +0900

    docs(desc): add read me

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  envDir: path.resolve(__dirname, './env'),
 });
